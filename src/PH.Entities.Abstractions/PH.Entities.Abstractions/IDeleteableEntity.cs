bd2a3e0fc2b5af026c14899d1f9aa9e10cd7be9a
Paolo Innocenti <paonath@gmail.com>
2023-01-12 06:39:57 +0100

    Done v.0.0.1

---
+++ src/PH.Entities.Abstractions/PH.Entities.Abstractions/IDeleteableEntity.cs
@@ -0,0 +1,27 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace PH.Entities.Abstractions
+{
+	/// <summary>
+	///   Entity with <see cref="Deleted" /> property for "soft-deleting"
+	/// </summary>
+	/// <typeparam name="TKey">The type of the key.</typeparam>
+	/// <seealso cref="PH.Entities.Abstractions.IEntity&lt;TKey&gt;" />
+	public interface IDeleteableEntity<TKey> : IEntity<TKey> where TKey : IEquatable<TKey>
+	{
+		/// <summary>
+		///   Gets or sets a value indicating whether this <see cref="IDeleteableEntity{TKey}" /> is deleted.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if deleted; otherwise, <c>false</c>.
+		/// </value>
+		bool Deleted { get; set; }
+	}
+}
---
+++ src/PH.Entities.Abstractions/PH.Entities.Abstractions/IEntity.cs
@@ -0,0 +1,26 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace PH.Entities.Abstractions
+{
+	/// <summary>
+	///   Entity with <see cref="Id" /> property as Primary Key
+	/// </summary>
+	/// <typeparam name="TKey">The type of the key.</typeparam>
+	public interface IEntity<TKey> where TKey : IEquatable<TKey>
+	{
+		/// <summary>
+		///   Gets or sets the primary key identifier.
+		/// </summary>
+		/// <value>
+		///   The identifier.
+		/// </value>
+		TKey Id { get; set; }
+	}
+}
